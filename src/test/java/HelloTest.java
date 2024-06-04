f99004fd4e43d51d35a814f0ca23364eaeca9cd5
sullis <github@seansullivan.com>
2024-06-04 09:05:18 -0700

    add src

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class HelloTest {
+  @Test
+  void testHello() {
+    // todo
+  }
+}
